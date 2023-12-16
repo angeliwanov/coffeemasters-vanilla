23a0259b2f94bed9ca65902611582c7e6d1f79c5
Angel Ivanov <angel.iwanov@gmail.com>
2023-12-16 14:36:55 +0200

    Routering ShadowDOM WebComponents

---
+++ services/Router.js
@@ -0,0 +1,56 @@
+const Router = {
+  init: () => {
+    document.querySelectorAll("a.navlink").forEach((a) => {
+      a.addEventListener("click", (event) => {
+        event.preventDefault();
+        const url = event.target.getAttribute("href");
+        Router.go(url);
+      });
+    });
+
+    //Event Handler for URL changes
+    window.addEventListener("popstate", (event) => {
+      Router.go(event.state.route, false);
+    });
+
+    //Check the initial URL
+    Router.go(location.pathname);
+  },
+  go: (route, addToHistory = true) => {
+    console.log(`Going to ${route}`);
+
+    if (addToHistory) {
+      history.pushState({ route }, "", route);
+    }
+
+    let pageElement = null;
+
+    switch (route) {
+      case "/":
+        pageElement = document.createElement("menu-page");
+        break;
+      case "/order":
+        pageElement = document.createElement("order-page");
+        break;
+      default:
+        if (route.startsWith("/product-")) {
+          pageElement = document.createElement("details-page");
+          const paramId = route.substring(route.lastIndexOf("-") + 1);
+          pageElement.dataset.productId = paramId;
+        }
+    }
+
+    if (pageElement) {
+      const cache = document.querySelector("main");
+      cache.innerHTML = "";
+      document.querySelector("main").appendChild(pageElement);
+      window.scrollX = 0;
+      window.scrollY = 0;
+    } else {
+      //browser console - app.router.go('/somewhere')
+      document.querySelector("main").innerHTML = "Oups, 404!";
+    }
+  },
+};
+
+export default Router;
